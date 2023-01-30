539fdd4d01246706df52241ba270dfa79cda1d96
Humayra Mahboob <humayra.mahboob01@myhunter.cuny.edu>
2023-01-30 10:43:22 -0500

    updated

--- leap.cpp
+++ leap.cpp
@@ -15,16 +15,16 @@ int main() {
 	cout << "Enter year:";
 	cin >> year;
 	if (year % 4 != 0){
-		cout << "Common year"<<endl;
+		cout << "Common year \n";
 	}
 	else if(year % 100 != 0) {
-		cout << "Leap year"<<endl;
+		cout << "Leap year \n";
 	}
 	else if(year % 400 != 0) {
-		cout << "Common year"<<endl;
+		cout << "Common year \n";
 	}
 	else {
-		cout << "Leap year"<<endl;
+		cout << "Leap year \n";
 	}
 	return 0;
 }
